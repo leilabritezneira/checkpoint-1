f01ab5a9acaeafec8a98859dd58193a559231883
Leila Britez Neira <103675851+leilabritezneira@users.noreply.github.com>
2022-08-13 02:04:35 +0200

    correcion de la validacion

--- scripts/form.js
+++ scripts/form.js
@@ -1,23 +1,34 @@
 const form = document.getElementById("form");
 const d = document;
 
+const validateName = (name) =>{
+  const regExUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  const validate = regExUserName.test(name) ? true : false
+  validate ? validate : alert(("El nombre solo debe incluir letras"))
+  return validate
+}
+
+const validateEmail = (email) =>{
+  const regExUserEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+  const validate = regExUserEmail.test(email) ? true : false
+  validate ? validate : alert("El e-mail ingresado es incorrecto, por favor ingrese uno correcto.")
+  return validate
+}
+
+const validateComments = (comments) => {
+  const validate = comments.length > 10 & comments.length < 250 ? true : false
+  validate ? validate : alert("El comentario debe tenerun mínimo de 10 caracteres y máximo 250 caracteres.")
+  return validate
+}
+
 form.addEventListener("submit", (e) =>{
   e.preventDefault();
 
   const user = d.getElementById("user").value;
   const email = d.getElementById("email").value;
   const comments = d.getElementById("comments").value; 
-  // REgEx for user and email valitation
-  const regExUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-  const regExUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)@[a-z0-9-]+(\.[a-z0-9-]+)(\.[a-z]{2,15})$/;
-  // user Name validation
-  if(!regExUserName.test(user)) {
-    console.error("El nombre solo debe incluir letras")
-  }
-  // email validation
-  if(!regExUserEmail.test(email)) {
-    console.error("El e-mail debe tener @, un . y al menos dos caracteres luego del .");
-  }   
-  console.table(`user: ${user} \nemail: ${email} \ncomments: ${comments}`);
-});
 
+  validateName(user) & validateEmail(email) & validateComments(comments) 
+  ? console.table(`user: ${user} \nemail: ${email} \ncomments: ${comments}`) : console.log("Error en el envío"); 
+
+});
